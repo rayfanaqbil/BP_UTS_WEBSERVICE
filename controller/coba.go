2915e61b2056c984492acc03c84c22b5517cf6bd
rayfanaqbil <rayfana09@gmail.com>
2024-05-10 03:27:16 +0700

    jajaja

--- controller/coba.go
+++ controller/coba.go
@@ -1,19 +1,22 @@
 package controller
 
 import (
-
-    "github.com/aiteung/musik"
-    "github.com/gofiber/fiber/v2"
-    adawong "github.com/rayfanaqbil/BE_UTS_Webservice"
-
+	"github.com/aiteung/musik"
+	"github.com/gofiber/fiber/v2"
+	adawong "github.com/rayfanaqbil/BE_UTS_Webservice"
 )
 
 func Homepage(c *fiber.Ctx) error {
-    ipaddr := musik.GetIPaddress()
-    return c.JSON(ipaddr)
+	ipaddr := musik.GetIPaddress()
+	return c.JSON(ipaddr)
 }
 
 func GetGetAllGames(c *fiber.Ctx) error {
-    ps := adawong.GetAllGames()
-    return c.JSON(ps)
+	gem, err := adawong.GetAllGamme()
+	if err != nil {
+
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.JSON(gem)
 }
